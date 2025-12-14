1a8c1c810e3d807d63afb7636fdc6a10fcc01b86
Lovish_10 <lovishg10@gmail.com>
2025-12-14 22:40:17 +0530

    modified:   Project Covering Chapter 0 to Chapter 8 LearnCPP/include/constants.h

--- Project Covering Chapter 0 to Chapter 8 LearnCPP/include/constants.h
+++ Project Covering Chapter 0 to Chapter 8 LearnCPP/include/constants.h
@@ -1,5 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
 namespace Const{
     // i can define other constants but its enough and I'm lazy 
     inline constexpr double gravity = 9.8;
 
 }
+
+#endif
