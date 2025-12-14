ffe3f55ceb6e66766c8ca9918be57af57f6288b0
Lovishg10 <81292802+Lovishg10@users.noreply.github.com>
2025-12-14 18:44:04 +0530

    Enhance header.h with comments on extern usage
    Added comments explaining the use of 'extern' keyword in header file.

--- Understandi External Internal Inline/include/header.h
+++ Understandi External Internal Inline/include/header.h
@@ -2,7 +2,8 @@
 #define HEADER_CONST
 
 /* remove extern keyword and you will get error, with extern you are sayin to compiler don't worry there is definition  of these constants  somewhere else, but this won't work with constexp , you will have to initialise it during declaration*/
-
+/*another important thing, if I donot use extern and initialise the constants, then each file that I include this header will get its own copy of these variables, 
+which in large porjects may not be good, hence then also extern keyword is viable option*/
 namespace Constants{
     extern const double gravity;
     extern const double coeff;
